c45fdd14fb643eb21d24e6e9eb28203d74b70414
RHUTHVIK DENDUKURI <rhuthvik.d@gmail.com>
2024-05-06 23:22:45 -0400

    Uploaded updated code

--- snake2.py
+++ snake2.py
@@ -3,9 +3,10 @@ import imutils
 import numpy as np
 import math
 from tkinter import messagebox as mb
-from random import randint,shuffle
+import random
 
 # Snake game in Python
+
 
 WIN_SCORE = 10
 MAX_OBSTACLES = 5
@@ -35,6 +36,7 @@ def drawImageOverFrame(frame, image, imageLocation):
     frame[(imageLocation[1] - (imageSize[1]//2)) : (imageLocation[1] + (imageSize[1]//2)), 
             (imageLocation[0] - (imageSize[0]//2)) : (imageLocation[0] + (imageSize[0]//2))] = image
 
+# Class for running the game logic
 class SnakeGame:
     def __init__(self, foodImagePath, obstacleImagePath) -> None:
         self.points = []
@@ -56,19 +58,22 @@ class SnakeGame:
         self.foodLocation = 0, 0
         self.generateRandomFoodLocation()
     
+    # Randomly generate obstacles
     def generateObstacles(self):
         self.obstacleLocations = []
         while len(self.obstacleLocations) < self.obstacleCount:
-            temp_point = (randint(50, VIDEO_WIDTH-50), randint(50, VIDEO_HEIGHT-50))
+            temp_point = (random.randint(50, VIDEO_WIDTH-50), random.randint(50, VIDEO_HEIGHT-50))
             if all(distance(temp_point,p) >= MIN_DISTANCE_BW_OBSTACLES for p in self.obstacleLocations):
                 self.obstacleLocations.append(temp_point)
 
+    # Randomly generate food location
     def generateRandomFoodLocation(self):
         while True:
-            self.foodLocation = randint(50, VIDEO_WIDTH-50), randint(50, VIDEO_HEIGHT-50)
+            self.foodLocation = random.randint(50, VIDEO_WIDTH-50), random.randint(50, VIDEO_HEIGHT-50)
             if all(distance(self.foodLocation, obstacle) > MIN_DISTANCE_FOOD_OBSTACLE for obstacle in self.obstacleLocations):
                 break
     
+    # Draw snake, food and obstacles
     def drawObjects(self, image):
         # Draw snake
         if self.points:
@@ -86,6 +91,7 @@ class SnakeGame:
         
         return image
 
+    # Reset game variables
     def reset(self):
         self.points = []
         self.lengths = []
@@ -99,12 +105,12 @@ class SnakeGame:
         self.generateObstacles()
         self.generateRandomFoodLocation()
 
-    
+    # Called when game is finished
     def gameFinished(self, frame, win: bool):
         if win:
             cv2.putText(frame, 'YOU WIN!!', (100, 250), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 3)
         else:
-            cv2.putText(frame, 'GAME OVER!!', (100, 250), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 3)
+            cv2.putText(frame, 'GAME OVER!!', (30, 250), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 3)
         cv2.imshow("Camera", frame)
         res = mb.askquestion('Exit Application', 'Play again?')       
         if res == 'yes' :
@@ -113,7 +119,7 @@ class SnakeGame:
             global playGame
             playGame = False
         
-
+    # Actual game loop is implemented here
     def updateSnake(self, image, NewHeadLocation):
         self.points.append(NewHeadLocation)
         dist_to_last_head = distance(self.previousHead, NewHeadLocation)
@@ -150,7 +156,7 @@ class SnakeGame:
         # Check collision with snake body
         check_pts = np.array(self.points[:-6], np.int32)
         check_pts = check_pts.reshape((-1,1,2))
-        cv2.polylines(image, [check_pts], False, (0,255,0), 3)
+        cv2.polylines(image, [check_pts], False, (255,0,0), 0)
         minimum_dist = cv2.pointPolygonTest(check_pts, NewHeadLocation, True)
         if -1 <= minimum_dist<= 1:
             self.gameFinished(image, False)
@@ -161,9 +167,8 @@ class SnakeGame:
         
         return image
 
-
+# Function for erosion
 def erode(mask, kernel_size=(3,3), iterations=1):
-    kernel = np.ones(kernel_size, np.uint8)
     eroded_mask = mask.copy()
     for _ in range(iterations):
         eroded_mask = np.minimum.reduce([
@@ -182,122 +187,83 @@ def dilate(mask, iterations=2):
         dilated_mask[1:-1, 1:-1] |= dilated_mask[1:-1, 2:]
     return dilated_mask
 
-### ONLY USED FOR WELZL
-
-# Structure to represent a 2D point
-class Point:
-    def __init__(self,X=0,Y=0) -> None:
+# 2D point for welzl's algorithm
+class Point_W:
+    def __init__(self, X=0, Y=0) -> None:
         self.X=X
         self.Y=Y
 
-# Structure to represent a 2D circle
-class Circle:
-    def __init__(self,c=Point(),r=0) -> None:        
+# Circle for welzl'a algorithm
+class Circle_W:
+    def __init__(self, c=Point_W(), r=0) -> None:        
         self.C=c
         self.R=r
 
-# Function to return the euclidean distance
-# between two points
-def dist(a: Point, b: Point):
-    return math.sqrt(pow(a.X - b.X, 2) + pow(a.Y - b.Y, 2))
+# Distance between 2 point_w
+def dist_2points(a: Point_W, b: Point_W) -> float:
+    return math.sqrt((a.X - b.X)**2 + (a.Y - b.Y)**2)
 
-# Function to check whether a point lies inside
-# or on the boundaries of the circle
-def is_inside(c: Circle, p: Point):
-    return dist(c.C, p) <= c.R
+# Check if point is inside circle
+def point_inside_circle(c: Circle_W, p: Point_W) -> bool:
+    return dist_2points(c.C, p) <= c.R
 
-# The following two functions are used
-# To find the equation of the circle when
-# three points are given.
-
-# Helper method to get a circle defined by 3 points
-def get_circle_center(bx, by, cx, cy):
-    B = bx * bx + by * by
-    C = cx * cx + cy * cy
-    D = bx * cy - by * cx
-    return Point((cy * B - by * C) / (2 * D), (bx * C - cx * B) / (2 * D))
-
-# Function to return the smallest circle
-# that intersects 2 points
-def circle_from1(A: Point, B: Point):
-    # Set the center to be the midpoint of A and B
-    C = Point((A.X + B.X) / 2.0, (A.Y + B.Y) / 2.0 )
-    # Set the radius to be half the distance AB
-    return Circle(C, dist(A, B) / 2.0 )
-
-# Function to return a unique circle that
-# intersects three points
-def circle_from2(A: Point, B: Point, C: Point):
-    I = get_circle_center(B.X - A.X, B.Y - A.Y, C.X - A.X, C.Y - A.Y)
-    I.X += A.X
-    I.Y += A.Y
-    return Circle(I, dist(I, A))
-
-# Function to check whether a circle
-# encloses the given points
-def is_valid_circle(c: Circle, P: list[Point]):
-    # Iterating through all the points
-    # to check  whether the points
-    # lie inside the circle or not
+# Check if points P lie in circle
+def is_circle_valid(c: Circle_W, P: list[Point_W]):
     for p in P:
-        if (not is_inside(c, p)):
+        if (not point_inside_circle(c, p)):
             return False
     return True
 
-# Function to return the minimum enclosing
-# circle for N <= 3
-def min_circle_trivial(P: list[Point]):
-    assert(len(P) <= 3)
-    if not P :
-        return Circle() 
-    elif (len(P) == 1) :
-        return Circle(P[0], 0) 
-    elif (len(P) == 2) :
-        return circle_from1(P[0], P[1])
-    # To check if MEC can be determined
-    # by 2 points only
+# Get circle for trivial cases: len(P) <= 3
+def get_circle_trivial(P: list[Point_W]):
+    if not P:
+        return Circle_W() 
+    elif (len(P) == 1):
+        return Circle_W(P[0], 0) 
+    elif (len(P) == 2):
+        return get_circle_2points(P[0], P[1])
     for i in range(3):
         for j in range(i + 1,3):
-            c = circle_from1(P[i], P[j])
-            if (is_valid_circle(c, P)):
+            c = get_circle_2points(P[i], P[j])
+            if (is_circle_valid(c, P)):
                 return c
-    return circle_from2(P[0], P[1], P[2])
+    return get_circle_3points(P[0], P[1], P[2])
 
-# Returns the MEC using Welzl's algorithm
-# Takes a set of input points P and a set R
-# points on the circle boundary.
-# n represents the number of points in P
-# that are not yet processed.
-def welzl_helper(P: list[Point], R: list[Point], n: int):
-    # Base case when all points processed or |R| = 3
+# Returns smallest circle with the 2 points on boundary
+def get_circle_2points(A: Point_W, B: Point_W):
+    Center = Point_W((A.X + B.X) / 2.0, (A.Y + B.Y) / 2.0 )
+    return Circle_W(Center, dist_2points(Center, A))
+
+# Returns smallest circle with the 3 points on boundary
+def get_circle_3points(A: Point_W, B: Point_W, C: Point_W):
+    baX = B.X - A.X
+    baY = B.Y - A.Y
+    caX = C.X - A.X
+    caY = C.Y - A.Y
+    tB = baX**2 + baY**2
+    tC = caX**2 + caY**2
+    tD = baX*caY - baY*caX
+    Center = Point_W(A.X + (caY*tB - baY*tC)/(tD*2), A.Y + (baX*tC - caX*tB)/(tD*2))
+    return Circle_W(Center, dist_2points(Center, A))
+
+# Recursive welzl helper function
+def welzl_helper(P: list[Point_W], R: list[Point_W], n: int):
     if (n == 0 or len(R) == 3) :
-        return min_circle_trivial(R)
-    # Pick a random point randomly
-    idx = randint(0,n-1)
-    p = P[idx]
-    # Put the picked point at the end of P
-    # since it's more efficient than
-    # deleting from the middle of the vector
-    P[idx], P[n - 1]=P[n-1],P[idx]
-    # Get the MEC circle d from the
-    # set of points P - :p
-    d = welzl_helper(P, R.copy(), n - 1)
-    # If d contains p, return d
-    if (is_inside(d, p)):
-        return d
-    # Otherwise, must be on the boundary of the MEC
-    R.append(p)
-    # Return the MEC for P - :p and R U :p
-    return welzl_helper(P, R.copy(), n - 1)
- 
-def welzl(P: list[Point]):
+        return get_circle_trivial(R)
+    r_index = random.randint(0,n-1)
+    random_point = P[r_index]
+    P[r_index],P[n-1] = P[n-1],P[r_index]
+    temp_C = welzl_helper(P, R.copy(), n-1)
+    if (point_inside_circle(temp_C, random_point)):
+        return temp_C
+    R.append(random_point)
+    return welzl_helper(P, R.copy(), n-1)
+
+# Applies Welzl's algorithm on points P
+def welzl(P: list[Point_W]):
     P_copy = P.copy()
-    shuffle(P_copy)
+    random.shuffle(P_copy)
     return welzl_helper(P_copy, [], len(P_copy))
-
-
-
-
 
 playGame = True
 game = SnakeGame("apple.jpg", "hurdle1small.jpg")
@@ -310,13 +276,14 @@ while playGame:
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
 
+    # Image preprocessing
     img = cv2.GaussianBlur(frame, (11, 11), 2)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     mask = ((np.logical_and(np.all(img >= GREEN_LOWER_THRESHOLD, axis = -1), np.all(img <= GREEN_UPPER_THRESHOLD, axis = -1))).astype(np.uint8))*255
     mask = erode(mask)
     mask = dilate(mask)
 
-    # find contours
+    # Find contours in image
     contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
 
@@ -325,19 +292,22 @@ while playGame:
         # Assuming ball_contour is the largest contour obtained from the binary mask image
 
         # Convert the contour to a list of points
-        points = [Point(x[0][0], x[0][1]) for x in ball_contour]
+        points = [Point_W(x[0][0], x[0][1]) for x in ball_contour]
 
         # Find the minimum enclosing circle using Welzl's algorithm
         min_enclosing_circle = welzl(points)
         center = (int(min_enclosing_circle.C.X), int(min_enclosing_circle.C.Y))
         radius = int(min_enclosing_circle.R)
 
+        # If radius big enough, run game loop
         if radius > 10:
             frame = game.updateSnake(frame, center)
+        # otherwise, only display game objects
         else:
             frame = game.drawObjects(frame)
     else:
         frame = game.drawObjects(frame)
+    
     cv2.imshow("Camera", frame)
     cv2.imshow("Mask", mask)
     if cv2.waitKey(1) == 27:
